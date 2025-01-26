4e6d58502fa538a53ce872697c86a97d8ef1542f
soufiane2001 <67438275+soufiane2001@users.noreply.github.com>
2025-01-26 16:27:20 +0100

    Update About.js

--- src/about/About.js
+++ src/about/About.js
@@ -21,7 +21,7 @@ const About = () => {
           <p>
           I'm Soufiane, a passionate and skilled web and mobile developer specializing in modern technologies like React.js and React Native. With a strong focus on crafting intuitive and responsive user interfaces, I ensure that every application I develop offers an exceptional user experience across all devices. My expertise extends to backend development with Laravel, where I design and implement secure, scalable, and efficient server-side logic to complement the front-end. With a deep understanding of the full software development lifecycle, I am adept at translating complex requirements into functional, high-quality solutions. My approach to development emphasizes clean code, best practices, and continuous learning to stay at the forefront of industry trends. Whether it’s building mobile applications or robust web platforms, I am committed to delivering projects that exceed expectations and drive success
           </p>
-          <button className="download-button"onClick={() =>  window.location.href =`${process.env.PUBLIC_URL}/cv25.pdf`} >Download CV</button>
+          <button className="download-button"onClick={() =>  window.location.href =`${process.env.PUBLIC_URL}/Cv25.pdf`} >Download CV</button>
         </div>
       </div>
     </div>
