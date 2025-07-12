9cd57c96c3905e780d73dec86161d9dd8f2cf397
soufiane2001 <67438275+soufiane2001@users.noreply.github.com>
2025-07-12 19:20:06 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Testimonials from './Testimonials/Testimonials';
 import Skills from './skills/Skills';
 import Experience from './Experience/Experience';
 import About from './about/About';
-import Footer from '../Footer';
+import Footer from './Footer';
 
 function Home(){
   return (
